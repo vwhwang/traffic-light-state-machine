ef23eb6f91ed24b20f6ee390275cc51c73c43362
vwhwang <vwhwang38@gmail.com>
2020-05-17 11:18:53 -0700

    added states

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -2,7 +2,25 @@ import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
   // Your state machine blueprint here
-
+  id: "lightToggle",
+  initial: "red", 
+  states: {
+    red:{
+      on : {
+        changeLight: "green"
+      }
+    },
+    green:{
+      on : {
+        changeLight: "yellow"
+      }
+    },
+    yellow: {
+      on : {
+        changeLight: "red"
+      }
+    } 
+  }
 });
 
 
